835560879f3f1ed37d39d7ad33af965e285c9dbe
Rene-Michel99 <renemichel856@gmail.com>
2021-06-22 02:35:09 -0300

    Atualizacao da seccao de projetos

---
+++ src/js/data.js
@@ -0,0 +1,59 @@
+function get_data(){
+  return [
+    {name:"Randomizer",lang:"Android",lang_icon:"./src/icons/android.png",prt_icon:"./src/icons/random-icon-generator.png",text:"App de randomização para a plataforma android desenvolvido usando a linguagem java com o android studio."},
+    {name:"Fish Tank",lang:"Javascript",lang_icon:"./src/icons/javascript.png",prt_icon:"./src/icons/aquarium.jpg",text:"Página Web que simula um pequeno aquário e trás informações a respeito dos peixes ao clicar neles, foi feito com javascript para movimentação e escolha dos peixes e HTML e CSS."},
+    {name:"Music Website",lang:"Node.js",
+    lang_icon:"./src/icons/node.jpg",
+    prt_icon:"./src/icons/channels.jpg",
+    text:"Sistema web para escutar Música com backend em Nodejs e frontend Bootstrap utlizando bootstrap para a estilização da página e nodejs como backend, o sistema possui login e cadastro e utiliza o mongoose para se conectar ao MongoDB e validar cadastro e login."},
+    {
+      name:"Yugioh Random Card",
+      lang:"JavaScript",
+      lang_icon:"./src/icons/javascript.png",
+      prt_icon:"./src/icons/yugioh-icon-73.png",
+      text:"Página Web feita com HTML e CSS e JavaScript para retornar cartas aleatórias de Yugioh e mostrar ao usuário."
+    },
+    {
+      name:"Web Server Python",
+      lang:"Python",
+      lang_icon:"./src/icons/python.png",
+      prt_icon:"./src/icons/webserver.png",
+      text:"Um servidor Web feito totalmente em Python sem utilizar pacotes como Django e HTTP já existentes no python."
+    },
+    {
+      name:"Batalha Naval com IA",
+      lang:"Python",
+      lang_icon:"./src/icons/python.png",
+      prt_icon:"./src/icons/batalha-naval.jpg",
+      text:"Jogo de batalha naval utilizando algoritmos de machine learning treinados do zero a partir de um dataset criado, utlizando 2 modelos para comparção de desempenho entre ambos um deles sendo utilizado o algoritmo KNN e o outro Naive Bayes"
+    },
+    {
+      name:"DPLL SAT Solver",
+      lang:"Python",
+      lang_icon:"./src/icons/python.png",
+      prt_icon:"./src/icons/dpll.png",
+      text:"Algoritmo DPLL desenvolvido em python para solução do problema SAT de fórmulas lógicas no padrão FNC com otimização de heurísticas."
+    },
+    {
+      name:"Assistente Virtual Minerva",
+      lang:"Python",
+      lang_icon:"./src/icons/python.png",
+      prt_icon:"./src/icons/Wave_IA.gif",
+      text:"Assistente virtual desenvolvida em python utilizando algoritmos de deep learning treinados do 0, possuindo uma rede neural CNN para reconhecimento de gestos e outra para conversação com vários comandos de voz."
+    },
+    {
+      name:"Note#",
+      lang:"C#",
+      lang_icon:"./src/icons/csharp.png",
+      prt_icon:"./src/icons/tasks.png",
+      text:"Sistema para criação de notas e tarefas desenvolvido em C# e utilizando como banco de dados o MySql."
+    },
+    {
+      name:"Previsão do Clima",
+      lang:"C#",
+      lang_icon:"./src/icons/csharp.png",
+      prt_icon:"./src/icons/weather.jpg",
+      text:"Simples interface para visualização da previsão do clima desenvolvido em C# e utilizando técnica de web scrappe para coletar os dados dinamicamente."
+    }
+  ];
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,11 @@
+var projects = get_data();
+var pos = 0;
+var is_animating = false;
+
 function drop_btn() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
-  
+
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
@@ -14,4 +18,42 @@ function drop_btn() {
         }
       }
     }
+}
+
+function next_btn(){
+  pos += 1;
+  if(pos >= projects.length)
+    pos = 0;
+
+  document.getElementById("lang-icon").src = projects[pos].lang_icon;
+  document.getElementById("prt-icon").src = projects[pos].prt_icon;
+
+  const title_prt = document.getElementById("title-prt");
+  const text = document.getElementById("desc");
+
+  const holder = $("#project-holder");
+  holder.animate({opacity: '0.3'}, "medium");
+  holder.animate({opacity: '1'},"medium");
+
+  title_prt.innerHTML = projects[pos].name;
+  text.innerHTML = projects[pos].text;
+}
+
+function prev_btn(){
+  pos -= 1;
+  if(pos < 0)
+    pos = projects.length - 1;
+
+  document.getElementById("lang-icon").src = projects[pos].lang_icon;
+  document.getElementById("prt-icon").src = projects[pos].prt_icon;
+
+  const title_prt = document.getElementById("title-prt");
+  const text = document.getElementById("desc");
+
+  const holder = $("#project-holder");
+  holder.animate({opacity: '0.3'}, "medium");
+  holder.animate({opacity: '1'},"medium");
+
+  title_prt.innerHTML = projects[pos].name;
+  text.innerHTML = projects[pos].text;
 }
